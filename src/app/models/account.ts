e409ee68403954e626fb7e9736120839532c7b1b
Angel Leon <angel@leanware.io>
2023-06-09 12:51:22 -0500

    added model and environment

---
+++ src/app/models/account.ts
@@ -0,0 +1,9 @@
+export class Account {
+  Status: number;
+  UserType: string;
+  Email: string;
+  FirstName: string;
+  LastName: string;
+  CompanyName: string;
+  Token?: string;
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://lab.app.invertebrado.co/api',
+};
