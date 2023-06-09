5d6d9b150b18656250f73b9f1c4e595cd82c4785
Angel Leon <angel@leanware.io>
2023-06-09 11:28:34 -0500

    ready header and footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule],
   exports: [HeaderComponent, FooterComponent],
 })
 export class SharedModule {}
