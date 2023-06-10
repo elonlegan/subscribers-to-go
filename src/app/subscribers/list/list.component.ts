3dcd82d591202d33013b236d9bae645d768b0a41
Angel Leon <angel@leanware.io>
2023-06-10 10:48:26 -0500

    added sort and pagination

--- src/app/subscribers/list/list.component.ts
+++ src/app/subscribers/list/list.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { SubscriberList } from '@app/models';
+import { Component, ViewChild } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Subscriber, SubscriberList } from '@app/models';
 import { SubscribersService } from '@app/services/subscribers.service';
 import { debounceTime, first } from 'rxjs/operators';
 
@@ -9,17 +12,29 @@ import { debounceTime, first } from 'rxjs/operators';
   styleUrls: ['./list.component.scss'],
 })
 export class ListComponent {
-  searchText: string = '';
   subscriberList: SubscriberList;
   displayedColumns: string[] = [
     'PublicId',
     'Name',
     'Email',
+    'Area',
     'CountryName',
     'PhoneCodeAndNumber',
     'SubscriptionStateDescription',
   ];
-  dataSource = [];
+  dataSource: MatTableDataSource<Subscriber>;
+  loading: boolean = true;
+
+  searchText: string = '';
+  pageIndex: number = 0;
+  pageSize: number = 10;
+
+  @ViewChild(MatSort) sort: MatSort;
+  sortTypes = {
+    asc: 0,
+    desc: 1,
+    DEFAULT: 0,
+  };
 
   constructor(private subscribersService: SubscribersService) {}
 
@@ -27,27 +42,57 @@ export class ListComponent {
     this.subscribersService
       .get()
       .pipe(first())
-      .subscribe((subscriberList) => {
-        this.subscriberList = subscriberList;
-        console.log(this.subscriberList);
-
-        this.dataSource = subscriberList.Data;
+      .subscribe((subscriberList: SubscriberList) => {
+        this.setData(subscriberList);
       });
   }
 
+  ngAfterViewInit() {
+    this.sort.sortChange.subscribe(() => {
+      this.resetPaginator();
+      this.getFilteredData();
+    });
+  }
+
+  setData(subscriberList: SubscriberList) {
+    this.subscriberList = subscriberList;
+
+    this.dataSource = new MatTableDataSource<Subscriber>(subscriberList.Data);
+    this.dataSource.sort = this.sort;
+    this.loading = false;
+  }
+
+  resetPaginator() {
+    this.pageIndex = 0;
+  }
+
+  onPageChange(event: PageEvent) {
+    this.pageIndex = event.pageIndex;
+    this.pageSize = event.pageSize;
+    this.getFilteredData();
+  }
+
   onSearchChange() {
+    this.resetPaginator();
     this.getFilteredData();
   }
 
   getFilteredData() {
-    this.subscribersService
-      .get({ criteria: this.searchText })
-      .pipe(first())
-      .subscribe((filteredData) => {
-        this.subscriberList = filteredData;
-        console.log(this.subscriberList);
+    this.dataSource = new MatTableDataSource();
+    this.loading = true;
 
-        this.dataSource = filteredData.Data;
+    this.subscribersService
+      .get({
+        criteria: this.searchText,
+        page: this.pageIndex + 1,
+        count: this.pageSize,
+        sortOrder: this.sort.active || this.displayedColumns[0],
+        sortType:
+          this.sortTypes[this.sort.direction] || this.sortTypes['DEFAULT'],
+      })
+      .pipe(first())
+      .subscribe((filteredData: SubscriberList) => {
+        this.setData(filteredData);
       });
   }
 }
